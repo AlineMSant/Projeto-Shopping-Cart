c4cedda46af876d2220aac27846c647df9ba2be9
AlineMSant <alinems4120@gmail.com>
2023-01-16 20:27:32 -0300

    requisito 4 - inclui carregamento enquanto carrega a API e exclui elemento de carregamento quando retorna inf da API

--- src/main.js
+++ src/main.js
@@ -6,8 +6,22 @@ import { createProductElement } from './helpers/shopFunctions';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const sectionProducts = document.querySelector('.products');
 
+const loading = () => {
+  const newElement = document.createElement('p');
+  newElement.innerHTML = 'carregando...';
+  newElement.className = 'loading';
+  sectionProducts.appendChild(newElement);
+};
+
+// const removeLoading = () => {
+//   sectionProducts.removeChild(loadingElement);
+// };
+
 const addProducts = async () => {
+  loading();
   const arrayProducts = await fetchProductsList('computador');
+  // removendo elemento sem usar .innerHTML='' https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
+  document.querySelector('.loading').remove();
   arrayProducts.forEach((element) => sectionProducts
     .appendChild(createProductElement(element)));
 };
