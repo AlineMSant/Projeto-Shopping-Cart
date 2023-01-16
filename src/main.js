e4f52ec1f0005fb8802b5370848183f0da490601
AlineMSant <alinems4120@gmail.com>
2023-01-16 18:30:52 -0300

    requisito 3 - cria função que criae inclui produtos, utilizando computador como parametro de fetchProductsList

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const sectionProducts = document.querySelector('.products');
+
+const addProducts = async () => {
+  const arrayProducts = await fetchProductsList('computador');
+  arrayProducts.forEach((element) => sectionProducts
+    .appendChild(createProductElement(element)));
+};
+
+addProducts();
