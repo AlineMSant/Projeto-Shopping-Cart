e73939ca3ee63cf2fae35dbc2c44415f07a6d42c
AlineMSant <alinems4120@gmail.com>
2023-01-17 17:11:07 -0300

    requisito 5 - concluido

--- src/main.js
+++ src/main.js
@@ -13,13 +13,24 @@ const loading = () => {
   sectionProducts.appendChild(newElement);
 };
 
+const errorAPI = () => {
+  const newElementError = document.createElement('p');
+  newElementError.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  newElementError.className = 'error';
+  sectionProducts.appendChild(newElementError);
+};
+
 const addProducts = async () => {
-  loading();
-  const arrayProducts = await fetchProductsList('computador');
-  // removendo elemento sem usar .innerHTML='' https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
-  document.querySelector('.loading').remove();
-  arrayProducts.forEach((element) => sectionProducts
-    .appendChild(createProductElement(element)));
+  try {
+    loading();
+    const arrayProducts = await fetchProductsList('computador');
+    // removendo elemento sem usar .innerHTML='' https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
+    document.querySelector('.loading').remove();
+    arrayProducts.forEach((element) => sectionProducts
+      .appendChild(createProductElement(element)));
+  } catch {
+    errorAPI();
+  }
 };
 
 addProducts();
