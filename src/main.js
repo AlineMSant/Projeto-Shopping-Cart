eabdde7a9e5a111059c566451845fcd9836cd2f7
AlineMSant <alinems4120@gmail.com>
2023-01-18 13:16:29 -0300

    correção de linter

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ const sectionProducts = document.querySelector('.products');
 window.onload = getSavedCartIDs().map(async (element) => {
   const elementOl2 = document.querySelector('.cart__products');
   Promise.all(
-    elementOl2.appendChild(createCartProductElement(await fetchProduct(element))));
+    elementOl2.appendChild(createCartProductElement(await fetchProduct(element)))
+  );
 });
 
 const loading = () => {
